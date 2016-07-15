3d8356d449e3e2791e3dcd0d3958a3cd9b54ba02
Muhammed Thanish <mnmtanish@gmail.com>
2016-07-15 17:10:09 +0530

    Install react-native-storybook

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import { AppRegistry } from 'react-native'
+import * as storybook from '@kadira/react-native-storybook'
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+storybook.configure(function () {
+  // ...
+}, module)
 
-class pokealert extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('pokealert', () => pokealert);
+AppRegistry.registerComponent('pokealert', function () {
+  return storybook.getStorybookUI({
+    port: 9001,
+    host: 'localhost',
+  })
+})
